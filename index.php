4823e7fbbe71bc3228313bc92e891c7cc928f97d
PavelBondarenko <59204295+PavelBondarenko@users.noreply.github.com>
2020-03-31 21:10:36 +0300

    book

--- index.php
+++ index.php
@@ -23,9 +23,26 @@ Port: 3306
     $link = mysqli_connect("remotemysql.com", "sPJAkGFeRc", "ll4BvY7ufL")
         or die("Не удалось соединится с сервером");
 
-    mysqli_select_db($link, "book")
+    mysqli_select_db($link, "sPJAkGFeRc")
         or die("Не удалось выбрать БД");
     echo "Hello world!!!!вфывыфва!!";
+
+    $query = "SELECT * FROM book";
+    $result = mysqli_query($link, $query)
+        or die("Не удалось выполнить запрос");
+
+    echo "<table border = '1'>\n";
+    while ($row = mysqli_fetch_array($result, MYSQLI_BOTH)) {
+        echo "<tr>";
+        echo "<td> <a href= 'del.php?id=$row[0]'>" . $row[0] . "</a></td>";
+        echo "<td>" . $row[1] . "</td>";
+        echo "<td>" . $row[2] . "</td>";
+        echo "</tr>";
+    }
+    echo "</table>\n";
+
+    mysqli_free_result($result);
+    mysqli_close($link);
     ?>
 </body>
 
